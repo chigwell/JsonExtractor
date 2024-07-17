0f8d15cf57a44a9cfa0f88696e7b2781f14be231
Federico Torrielli <evilscript@protonmail.com>
2024-07-17 10:48:46 +0200

    reformat to adhere to the latest PEP

--- json_extractor/extractor.py
+++ json_extractor/extractor.py
@@ -1,5 +1,6 @@
 import ujson as json
 
+
 class JsonExtractor:
     @staticmethod
     def extract_valid_json(input_str):
@@ -7,16 +8,19 @@ class JsonExtractor:
         start_index = None
 
         for i, char in enumerate(input_str):
-            if char in '{[':
+            if char in "{[":
                 stack.append(char)
                 if len(stack) == 1:
                     start_index = i
-            elif char in '}]':
-                if stack and ((char == '}' and stack[-1] == '{') or (char == ']' and stack[-1] == '[')):
+            elif char in "}]":
+                if stack and (
+                    (char == "}" and stack[-1] == "{")
+                    or (char == "]" and stack[-1] == "[")
+                ):
                     stack.pop()
                     if not stack and start_index is not None:
                         try:
-                            return json.loads(input_str[start_index:i + 1])
+                            return json.loads(input_str[start_index : i + 1])
                         except json.JSONDecodeError:
                             start_index = None
 
